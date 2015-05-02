4c077462f27e9c1e3ad507c86743ddf92965715a
Michael Yee <michaelyee@att.net>
2015-05-02 02:57:00 -0700

    Updates to Receiver
    Main issue : According to Serial.println(theMessage); ,  the input from the Controller(Sender) is only sending 1 char or the receiver is only able to gather one char lines?

--- Quadcopter_Receiver_ArdGit/Quadcopter_Receiver_ArdGit.ino
+++ Quadcopter_Receiver_ArdGit/Quadcopter_Receiver_ArdGit.ino
@@ -1,3 +1,4 @@
+
 #include <Servo.h>
 #include <SPI.h>
 #include "RF24.h"
@@ -25,19 +26,28 @@ int decDir = 180; // for x - y changing position
 bool motorIn = true;
 byte send;
 const uint64_t pipe = 0xE8E8F0F0E1LL;
+byte addresses[][6] = {"1Node", "2Node"};
 int msg[15];
 
 // EDIT WHICH PINS THE RADIO WILL BE ON
-RF24 radio(9, 10); 
+RF24 radio(9, 10);
 
 
 void setup() {
 
-  Serial.begin(1000000); // open the serial port at 9600 bps
-  establishContact();
+  radio.begin();
+
+  Serial.begin(4800); // open the serial port at 9600 bps
+  //establishContact();
   // send a byte to establish contact until receiver responds
   //
 
+  radio.openWritingPipe(addresses[1]);
+  radio.openReadingPipe(1, addresses[0]);
+  //radio.openReadingPipe(1, addresses[0]);
+  radio.startListening();
+
+  /*
   pinMode(9, OUTPUT); // sets Pin 9 to Output [?]
   pinMode(10, OUTPUT); // sets Pin 10 to Output [?]
   pinMode(11, OUTPUT); // sets Pin 11 to Output [?]
@@ -52,37 +62,65 @@ void setup() {
   t.write(0); // set Servo T to speed 0
   u.write(0); // set Servo U to speed 0
   v.write(0); // set Servo V to speed 0
+  */
 
-  radio.begin();
-  radio.openReadingPipe(1, pipe);
-  radio.startListening();
 
 }
 
 void loop() {
+
+  String theMessage = "";
+  //char a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p;
 
   int signalInX = 0, signalInY = 0, signalInZ = 0;
   int motorInXL = 0, motorInXR = 0,
       motorInYF = 0, motorInYB = 0, motorInZ = 0;
   int motorInX = 0, motorInY = 0;
 
-  String theMessage = "";
+
   if (radio.available())
   {
-    radio.startListening();
-    radio.read(msg, 15);
 
     for (int i = 0; i < 15; i ++)
     {
+      radio.read(msg, 15);
+
       char theChar = msg[i];
+      
+      /*
+      char a = msg[0];
+      char b = msg[1];
+      char c = msg[2];
+      char d = msg[3];
+      char e = msg[4];
+      char f = msg[5];
+      char g = msg[6];
+      char h = msg[7];
+      char p = msg[8];
+      char j = msg[9];
+      char k = msg[10];
+      char l = msg[11];
+      char m = msg[12];
+      char n = msg[13];
+      char o = msg[14];
+      */
+      
+      
       if (theChar == ('C'))
       {
         break;
+
       }
-      theMessage += theChar;
       
+      String x = String(theChar);
+      theMessage += x;
+      
+      //theMessage = a+""+b+""+c+""+d+""+e+""+f+""+g+""+h+""+p+""+j+""+k+""+l+""+m+""+n+""+o;
+      //theMessage = ""+a;
     }
+    Serial.println(theMessage);
     
+    //delay(2500);
 
     xIndex = theMessage.indexOf("X");
     yIndex = theMessage.indexOf("Y");
@@ -123,66 +161,77 @@ void loop() {
       motorInYB = signalInY;
       motorInYF = signalInY - 10;
     }
-    
-    if(signalInZ < 0)
+
+    if (signalInZ < 0)
     {
-     motorInZ = 0; 
+      motorInZ = 0;
     }
     else
     {
-     motorInZ = signalInZ; 
+      motorInZ = signalInZ;
     }
-    
+
     motorInXL += signalInZ;
     motorInXR += signalInZ;
     motorInYF += signalInZ;
     motorInYB += signalInZ;
-    
-    if(motorInXL >= 180)
+
+    if (motorInXL >= 180)
       motorInXL = 179;
-    if(motorInXR >= 180)
+    if (motorInXR >= 180)
       motorInXR = 179;
-    if(motorInYB >= 180)
+    if (motorInYB >= 180)
       motorInYB = 179;
-    if(motorInYF >= 180)
+    if (motorInYF >= 180)
       motorInYF = 179;
-    
+
     s.write(motorInXL);
     t.write(motorInYF);
     u.write(motorInXR);
     v.write(motorInYB);
-    
+
     lastMotorInXL = motorInXL;
     lastMotorInXR = motorInXR;
     lastMotorInYF = motorInYF;
     lastMotorInYB = motorInYB;
-    
+
+
+
+    /*
     // begin writing outputs -- test
-    
+
     radio.stopListening();
     String sendMessage = x+y+z;
-    
+
     radio.openWritingPipe(pipe);
-    
+
     for (int i = 0; i < sendMessage.length(); i ++)
     {
       int charToSend[1];
       charToSend[0] = sendMessage.charAt(i);
       radio.write(charToSend, 15);
     }
-    
+
     msg[0] = 'C';  // sends a terminating string value
     radio.write(msg, 1);
 
-    radio.powerDown();
-    delay(1);
-    radio.powerUp();
-    
-    // end writing 
-    
-    
+    */
+    //radio.powerDown();
+    //delay(1);
+    //radio.powerUp();
+
+    // end writing
+
+
   }
+  else
+  {
+    Serial.println("No Radio.");
+  }
+
 }
+
+
 
 void establishContact() {
   while (Serial.available() <= 0) {
